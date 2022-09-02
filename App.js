20ffd861d1c62aa9a18ede4c2d2ff1517c9b414a
AnnePe <anne.pelin@koodata.fi>
2022-09-02 13:26:07 +0300

    vaihdoin countin pois tilamuuttujasta globaaliksi muuttujaksi let count

--- App.js
+++ App.js
@@ -2,36 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect} from 'react';
 import { StyleSheet, Text, View, TextInput, Alert, Button, Image} from 'react-native';
 
+let lkm= 0;
 export default function App() {
   const [number, setNumber] = useState(0);//random
   const [anumber, setAnumber] = useState(0);//arvattu numero
-  const [count, setCount] = useState(1);//laskuri
-
-   
+  //const [count, setCount] = useState(1);//laskuri
+ 
+  
 const getRandomNumber = () => {  //random yhteenkertaan
   const xnumber = Math.floor(Math.random() * 100) + 1;
  setNumber(xnumber);
-  }
+   }
  
  const buttonPressed = () => { //counter
-   setCount(count + 1);
-  
+ //  setCount(count + 1);
+   lkm++;
+   console.log(number);
     if (anumber > number ){
-      Alert.alert('Your guess: ' + anumber + ' is too high ' + number); 
+      Alert.alert('Your guess: ' + anumber + ' is too high '); 
     
     } else if (anumber < number){
-      Alert.alert('Your guess: ' + anumber + ' is too low ' + number); 
-     
+      Alert.alert('Your guess: ' + anumber + ' is too low ' ); 
+         
     } else if (anumber == number){
-      Alert.alert('Congratulations! Your guess is correct number : ' + anumber +  ' at ' + count +' attempt'); 
-      setCount(0);
+      Alert.alert('Congratulations! Your guess is correct number : ' + anumber +  ' at ' +lkm+ ' attempt'); 
+      //setCount(1);
+      lkm=0;
       getRandomNumber();
-  }
-      
+    }
+ setAnumber('');
   };
   
  useEffect(() => {
     getRandomNumber(); // this will fire only on first render
+    
 }, []);
 
 
@@ -41,7 +45,7 @@ const getRandomNumber = () => {  //random yhteenkertaan
          <Image style={styles.image} source={{uri: 'https://www.sttinfo.fi/data/images/00304/be7db042-6b61-49f9-9bcd-7fd41b7bc35d.jpg'}} />
         <TextInput  placeholder='Number' keyboardType="numeric" style={styles.input} onChangeText={anumber => setAnumber(anumber)} value={anumber}/>
              
-        <Button onPress={buttonPressed} title="Make Guess" />
+        <Button onPress={buttonPressed} title="Make Guess !" />
        
       <StatusBar style="auto" />
     </View>
